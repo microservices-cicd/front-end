0c094cb0401e671d04d9422897471d8ae4f3b36a
Florin Peter <github@florin-peter.de>
2018-04-12 15:37:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,8 @@
   'use strict';
 
   var session      = require("express-session"),
-      RedisStore   = require('connect-redis')(session)
+      RedisStore   = require('connect-redis')(session),
+      redis        = require('redis');
 
   module.exports = {
     session: {
@@ -12,8 +13,10 @@
       saveUninitialized: true
     },
 
+    var client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST, { auth_pass: process.env.REDIS_PASS, tls: { servername: process.env.REDIS_HOST } });
+
     session_redis: {
-      store: new RedisStore({host: "session-db"}),
+      store: new RedisStore({client: client}),
       name: 'md.sid',
       secret: 'sooper secret',
       resave: false,
